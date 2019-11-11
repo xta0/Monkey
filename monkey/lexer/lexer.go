19187fa942212c8703766354ac823d3b75fc1fdb
Tao Xu <taox@fb.com>
2019-11-10 19:31:27 -0800

    Update(auto commit)

--- monkey/lexer/lexer.go
+++ monkey/lexer/lexer.go
@@ -22,13 +22,34 @@ func New(input string) *Lexer {
 // NextToken returns the next Lexer token
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhitespace()
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, string(l.ch))
+		if l.peekchar() == '=' {
+			ch := l.ch
+			l.readchar()
+			tok = newToken(token.EQ, string(ch)+string(l.ch))
+		} else {
+			tok = newToken(token.ASSIGN, string(l.ch))
+		}
 	case ',':
 		tok = newToken(token.COMMA, string(l.ch))
 	case '+':
 		tok = newToken(token.PLUS, string(l.ch))
+	case '-':
+		tok = newToken(token.MINUS, string(l.ch))
+	case '*':
+		tok = newToken(token.ASTERISK, string(l.ch))
+	case '/':
+		tok = newToken(token.SLASH, string(l.ch))
+	case '!':
+		if l.peekchar() == '=' {
+			ch := l.ch
+			l.readchar()
+			tok = newToken(token.NOT_EQ, string(ch)+string(l.ch))
+		} else {
+			tok = newToken(token.BANG, string(l.ch))
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, string(l.ch))
 	case ')':
@@ -39,12 +60,21 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, string(l.ch))
 	case '}':
 		tok = newToken(token.RBRACE, string(l.ch))
-
+	case '>':
+		tok = newToken(token.GT, string(l.ch))
+	case '<':
+		tok = newToken(token.LT, string(l.ch))
 	case 0:
 		tok = newToken(token.EOF, "")
 	default:
 		if isLetter(l.ch) {
-			tok = newToken(token.IDENT, l.readIdentifier())
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIndent(tok.Literal)
+			return tok
+		} else if isNumber(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, string(l.ch))
 		}
@@ -59,7 +89,14 @@ func (l *Lexer) readIdentifier() string {
 		l.readchar()
 	}
 	return l.input[position:l.position]
+}
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isNumber(l.ch) {
+		l.readchar()
+	}
+	return l.input[position:l.position]
 }
 
 func (l *Lexer) readchar() {
@@ -72,10 +109,26 @@ func (l *Lexer) readchar() {
 	l.readPosition++
 }
 
+func (l *Lexer) peekchar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+	return l.input[l.readPosition]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\n' || l.ch == '\r' || l.ch == '\t' {
+		l.readchar()
+	}
+}
+
 func newToken(tokenType token.TokenType, litral string) token.Token {
 	return token.Token{Type: tokenType, Literal: litral}
 }
 
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch >= 'Z' || ch == '_'
+}
+func isNumber(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- monkey/token/token.go
+++ monkey/token/token.go
@@ -1,13 +1,15 @@
 package token
 
+// TokenType xxx
 type TokenType string
 
+// Token xxx
 type Token struct {
 	Type    TokenType
 	Literal string
 }
 
-//token types
+// supported token types
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -17,8 +19,16 @@ const (
 	INT   = "INT"   //12345
 
 	//Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	PLUS     = "+"
+	MINUS    = "-"
+	ASTERISK = "*"
+	BANG     = "!"
+	ASSIGN   = "="
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
 
 	// Delimiters
 	COMMA     = ","
@@ -32,4 +42,27 @@ const (
 	//keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
+}
+
+// LookupIndent returns the token type of indent
+func LookupIndent(ident string) TokenType {
+	if identType, ok := keywords[ident]; ok {
+		return identType
+	}
+	return IDENT
+}
